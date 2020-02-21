ef7709df72e626bee22f1fb25865db33b4ae0efd
dylantknguyen <dylannguyen1000@gmail.com>
2020-02-21 09:59:37 -0500

    Cleaned code up

--- robot/shooter.py
+++ robot/shooter.py
@@ -5,39 +5,40 @@ from ctre import *
 
 
 class Range:
-    differentRangesInWords = []  # contains all the objects that have been created
+	# contains different ranges
+	rangeInWords = []
 
-	def __init__(self, shootingRangeInWords, topRPM, bottomRPM):
-		self.shootingRangeInWords = shootingRangeInWords  # name of the range
-		self.topRPM = topRPM  # speed of top rpm
-		self.bottomRPM = bottomRPM  # speed of bottom rpm
-        
-        # adds the current object to the objects that have been instantiated to "differentRangesInWords"
-        Range.differentRangesInWords.append(self)
+	def __init__(self, rangeInWords, topRPM, bottomRPM):
+		self.rangeInWords = rangeInWords  # name of the range
+		self.topRPM = topRPM  # rpm of top motors
+		self.bottomRPM = bottomRPM  # rpm of bottom motors
+
+
+	# adds the current object to the array of ranges
+	Range.rangeInWords.append(self)
 
 
 class Shooter:
-    def __init__(self):
-        # shooter motors and encoders
-        self.topShooter1Encoder = WPI_TalonSRX(1)
-        self.topShooter2 = WPI_VictorSPX(2)
-        self.bottomShooter1Encoder = WPI_TalonSRX(3)
-        self.bottomShooter2 = WPI_VictorSPX(4)
+	def __init__(self):
+		# shooter motors and encoders
+		self.topShooter1Encoder = WPI_TalonSRX(1)
+		self.topShooter2 = WPI_VictorSPX(2)
+		self.bottomShooter1Encoder = WPI_TalonSRX(3)
+		self.bottomShooter2 = WPI_VictorSPX(4)
 
-        # shooter motor groups
-        self.topMotors = wpilib.SpeedControllerGroup(self.topShooter1Encoder, self.topShooter2)
-        self.bottomMotors = wpilib.SpeedControllerGroup(self.bottomShooter1Encoder, self.bottomShooter2)
+		# shooter motor groups
+		self.topMotors = wpilib.SpeedControllerGroup(self.topShooter1Encoder, self.topShooter2)
+		self.bottomMotors = wpilib.SpeedControllerGroup(self.bottomShooter1Encoder, self.bottomShooter2)
 
-        # different ranges in words
-        shooterFar = Range('far', 0, 0)  # Set top and bottom RPMs
-        shooterMid = Range('mid', 0, 0)  # Set top and bottom RPMs
-        shooterShort = Range('short', 0, 0)  # Set top and bottom RPMs
-    
-	# call this function with the name of the range in words
-	# for example, you can call shootPreDefinedLengths('far')
-	def shootPreDefinedLengths(self, distanceInWords):
-		""" This method will set the rpm of the motors if the range in words matches the input
-		
+		# setting different ranges in words
+		shooterFar = Range('far', 0, 0)
+		shooterMid = Range('mid', 0, 0)
+		shooterShort = Range('short', 0, 0)
+
+	def shootPredefinedDistance(self, distanceInWords):
+		""" This method will call this function with the name of the range in words
+		For example, you can call shootPreDefinedDistance'far') to shoot the preset 'far' distance
+
 		:param distanceInWords:
 		:type distanceInWords: String
 
@@ -46,16 +47,16 @@ class Shooter:
 
 		# this loop will go through each instance made by the class
 		# and will find which instance's name is equlivalent to the distanceInWords
-		for instance in Range.differentRangesInWords:
-			if instance.shootingRangeInWords == distanceInWords:
+		for instance in Range.rangeInWords:
+			if instance.rangeInWords == distanceInWords:
 				setTopShooterRpm(instance.topRPM)  # sets the top rpm
 				setBottomShooterRpm(instance.bottomRPM)  # sets the bottom rpm
 
-    def shootAutonomous(self, distance):
-        # automatically shoot balls given distance
-        pass
+	def shootAutonomous(self, distance):
+		# automatically shoot balls given distance
+		pass
 
-	def convertVelocityToRpm(rawVelocity):
+	def velocityToRpm(rawVelocity):
 		""" This method will take in velocity and convert the velocity into rotations per minute
 
 		:param rawVelocity:
@@ -75,9 +76,9 @@ class Shooter:
 		:rtype rpm: float
 		"""
 
-		rawVelocity = self.topShooter1Encoder.getSelectedSensorVelocity()  # get velocity
+		rawVelocity = self.topShooter1Encoder.getSelectedSensorVelocity()  # get velocity from encoder
 		rpm = convertVelocityToRpm(rawVelocity)  # convert to rpm
-        return rpm
+		return rpm
 
 	def getBottomShooterRpm(self):
 		""" This method will return the rpm of the bottom shooter spped controller group
@@ -86,12 +87,12 @@ class Shooter:
 		:rtype rpm: float
 		"""
 
-        rawVelocity = self.bottomShooter1Encoder.getSelectedSensorVelocity()  # get velocity
-        rpm = convertVelocityToRpm(rawVelocity)  # conver to rpm
-        return rpm
+		rawVelocity = self.bottomShooter1Encoder.getSelectedSensorVelocity()  # get velocity
+		rpm = velocityToRpm(rawVelocity)  # convert to rpm
+		return rpm
 
 	def setTopShooterRpm(self, rpm):
-		self.topMotor.set(rpm)
-	
+		self.topMotors.set(rpm)
+
 	def setBottomShooterRpm(self, rpm):
-        self.bottomMotor.set(rpm)
+		self.bottomMotors.set(rpm)
